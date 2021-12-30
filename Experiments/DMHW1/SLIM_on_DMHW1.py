b3a996a211d5d9dd04e3f763639206fbea3847f4
jimmy778878 <56869343+jimmy778878@users.noreply.github.com>
2021-12-30 22:35:42 +0800

    Add files via upload

---
+++ Experiments/DMHW1/SLIM_on_DMHW1.py
@@ -0,0 +1,33 @@
+from skmine.itemsets import SLIM
+import csv
+import time
+
+input_file = "../Data/DMHW1_data.csv"
+output_file = "DMHW1_SLIM_patterns.txt"
+
+# 把資料轉成適當的格式
+with open(input_file, newline = "") as csvF:
+    rows = csv.reader(csvF)
+
+    header = next(rows)
+    items_dict = {}
+    for index, value in enumerate(header):
+        items_dict[index] = value
+
+    Data = []
+    for row in rows:
+        tmp = [items_dict[index] for index, value in enumerate(row) if value == "True"] 
+        Data.append(tmp)
+
+# 找 pattern sets
+start = time.time()
+output = SLIM(k=20000).fit(Data).discover(singletons=False, usage_tids=True)
+end = time.time()
+
+# 把找出來的 pattern sets 記錄起來
+with open(output_file, "w") as f:
+    for pattern in output.index:
+        f.write(str(pattern) + "\n")
+
+print("SLIM exetution time: ", int(end - start), " seconds")
+print("number of pattern set: ", len(output))
