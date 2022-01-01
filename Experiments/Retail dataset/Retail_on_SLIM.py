787335b5b98dfdf1a5e600fe9efdc4c78a43adc4
JacobLiu <47729040+kk25gb@users.noreply.github.com>
2022-01-01 21:33:23 +0800

    Update and rename RetailDataset_on_SLIM.py to Retail_on_SLIM.py

--- Experiments/Retail dataset/Retail_on_SLIM.py
+++ Experiments/Retail dataset/Retail_on_SLIM.py
@@ -1,6 +1,7 @@
 from skmine.itemsets import SLIM
 import time
 
+k_num = 50
 data = []
 with open('C:/Users/Jacob/Desktop/研究所/資料探勘/binaps/Data/Test_retail/retail.dat', 'r') as f:
     for line in f.readlines():
@@ -9,7 +10,7 @@ with open('C:/Users/Jacob/Desktop/研究所/資料探勘/binaps/Data/Test_retail
         data.append(line_li)
 
 start = time.process_time()
-result = SLIM(k=300).fit(data).discover(singletons=False, usage_tids=True)
+result = SLIM(k=k_num).fit(data).discover(singletons=False, usage_tids=True)
 end = time.process_time()
 print(result)
 print("執行時間：%f 秒" % (end - start))
